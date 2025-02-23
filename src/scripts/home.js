6dbb84aedffa9895ae09d68210aa614f8de137e6
jannalyx <janainapcj44@gmail.com>
2025-02-22 23:50:04 -0300

    update

---
+++ src/scripts/home.js
@@ -0,0 +1,30 @@
+console.log("Script carregado!");
+
+const texto = "Encontre Seu Novo Amigo!";
+let indice = 0;
+const velocidade = 100; 
+
+function efeitoMaquinaDeEscrever() {
+    if (indice < texto.length) {
+        document.getElementById("typewriter").innerHTML += texto.charAt(indice);
+        indice++;
+        setTimeout(efeitoMaquinaDeEscrever, velocidade);
+    }
+}
+
+window.addEventListener("load", efeitoMaquinaDeEscrever);
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+
+    if (!form) {
+        console.error("Formulário não encontrado!");
+        return;
+    }
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        alert("Mensagem enviada com sucesso! Entraremos em contato em breve.");
+        form.reset();
+    });
+});
--- src/scripts/popup_adocao.js
+++ src/scripts/popup_adocao.js
@@ -1,25 +1,20 @@
-    // Obter o modal
+
     const modal = document.getElementById("myModal");
 
-    // Obter o botão que abre o modal
     const btn = document.getElementById("myBtn");
 
-    // Obter os elementos que fecham o modal
     const closeBtns = document.querySelectorAll(".close");
 
-    // Abrir o modal ao clicar no botão
     btn.onclick = function () {
       modal.style.display = "block";
     };
 
-    // Fechar o modal ao clicar nos elementos de fechamento
     closeBtns.forEach((btn) => {
       btn.onclick = function () {
         modal.style.display = "none";
       };
     });
 
-    // Fechar o modal ao clicar fora dele
     window.onclick = function (event) {
       if (event.target == modal) {
         modal.style.display = "none";
